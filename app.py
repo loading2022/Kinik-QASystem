f751b929a0e6eefb9c276bf0844623658dcb13a5
ShihPing0803 <119124535+loading2022@users.noreply.github.com>
2024-08-03 23:51:13 +0800

    Change openai api key environment name

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import openai
 import io
 from openai import OpenAI
 
-openai.api_key = os.getenv("AZURE_OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT")
 openai.api_type = "azure"
 openai.api_version = os.getenv("AZURE_OPENAI_MODEL") 
